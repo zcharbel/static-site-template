dcfc8291956667b30853b23664f84f3561dfb83a
Zach Charbel <zach@zachcharbel.com>
2023-09-20 10:09:45 -0400

    Updates

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,53 +17,12 @@ module.exports = function(grunt) {
 				}
 			}
 		},
-		// Remove unused CSS from URLs (php, node, etc.)
-		// (Note that`nonull` must be true, or else Grunt
-		// removes remote paths that it can't find locally)
-		uncss: {
-		    dist: {
-			    options: {
-		            ignore: [
-						/(#|\.)baguetteBox(-[a-zA-Z]+)?/,
-						/\w\.in/,
-						'.fade',
-						'.collapse',
-						'.collapsed',
-						'.collapsing',
-						/(#|\.)navbar(-[a-zA-Z]+)?/,
-						/(#|\.)dropdown(-[a-zA-Z]+)?/,
-						/(#|\.)(open)/,
-						/disabled/,
-						/\.no-js/,
-						/\.defer/
-					]
-			    },
-		        files: [{
-		            src: ['production/index.html', 'production/folder/folder-test.html'],
-		            dest: 'production/assets/css/styles.min.css'
-		        }]
-		    }
-		},
 		// this takes your main js file and minifies it to save weight
 		uglify: {
 			my_target: {
 				files: {
 					'production/assets/js/main.min.js': ['src/assets/js/vendor/*.js', 'src/assets/js/main.js']
 				}
-			}
-		},
-		// this optimizes your images for when you are ready to release your website
-	    imagemin: {                          
-			dynamic: {
-				options: {
-					optimizationLevel: 7
-				},                    
-				files: [{
-					expand: true,                  
-					cwd: 'src/assets/imgs/',                   
-					src: ['**/*.{png,jpg,gif}'],   
-					dest: 'production/assets/imgs/'
-				}]
 			}
 		},
 		// this compiles your .php and "_includes" .php files into singular .html files
@@ -148,30 +107,13 @@ module.exports = function(grunt) {
 				}]
 			}
 		},
-		// this minifies the html 
-		minifyHtml: {
-	        dist: {
-		        options: {
-					removeComments: true,
-					collapseWhitespace: true
-				},
-	            files: [{
-					expand: true,   
-					cwd: 'production/',
-					src: ['**/*.html'],
-					dest: 'production/'	
-				}]
-	        }
-	    },
 	    // this is a local server for development purposes
 	    php: {
 	        dist: {
 	            options: {
 	                hostname: '127.0.0.1',
-	                port: 9000,
-	                base: 'src', // Project root 
-	                keepalive: false,
-	                open: false
+	                port: 5000,
+	                base: 'src'
 	            }
 	        }
 	    },
@@ -201,17 +143,14 @@ module.exports = function(grunt) {
 	
 	// loadNpmTasks bring in required grunt modules for use within this file
 	grunt.loadNpmTasks('grunt-contrib-sass');
-	grunt.loadNpmTasks('grunt-uncss');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-imagemin');
 	grunt.loadNpmTasks('grunt-contrib-rename');
 	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-php2html');
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-text-replace');
-	grunt.loadNpmTasks('grunt-minify-html');
 	grunt.loadNpmTasks('grunt-php');
 	grunt.loadNpmTasks('grunt-browser-sync');
 	
@@ -225,6 +164,6 @@ module.exports = function(grunt) {
 	// This is for debugging output code from PHP
 	grunt.registerTask('build', ['clean:production', 'sass', 'php2html', 'copy', 'clean:includes', 'clean:scss']);
 	// This is when you are ready to push to production. Includes minifying HTML, CSS and JS, optimizing images and updates file paths for .min.xxx extensions
-	grunt.registerTask('release', ['clean:production', 'sass',  'php2html', 'cssmin', 'imagemin', 'uglify', 'copyFontsFavs', 'replace', 'minifyHtml', 'clean:includes', 'uncss']);
+	grunt.registerTask('release', ['clean:production', 'sass',  'php2html', 'cssmin', 'imagemin', 'uglify', 'copyFontsFavs', 'replace', 'clean:includes']);
 	
 }
